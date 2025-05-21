afcf5b32ea1b7afc5bed5e7b3f8296a381c96c38
Jiayi Gao <143228908+JiayiGao888@users.noreply.github.com>
2025-05-21 00:45:13 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,3 @@
+LLM_API_KEY = "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"
+RESUME_PARSER_JSON_FILE = "llm_instructions/resume_parser_api.json"
+COVER_LETTER_JSON_FILE = "llm_instructions/cover_letter_api.json"
